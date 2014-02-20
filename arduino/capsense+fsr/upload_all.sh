5cc08c8b7fe723c236a973fcf7cab025e84e6713
Antoine Villeret <antoine.villeret@gmail.com>
2014-02-20 19:45:16 +0100

    more verbose script

--- arduino/capsense+fsr/upload_all.sh
+++ arduino/capsense+fsr/upload_all.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+ino clean
+echo Build the sketch...
+ino build -m micro
+
 for arduino in /dev/ttyACM* ; 
 do 
+echo upload to $arduino
 ino upload -m micro -p $arduino
-echo $?
 echo wait a little bit...
 sleep 1
 done
