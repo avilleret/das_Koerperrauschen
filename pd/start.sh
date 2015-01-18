8f21086d6ac10366b11d5e41b46f92ee39a10d3d
Antoine Villeret <antoine.villeret@gmail.com>
2015-01-18 22:16:04 +0100

    add startup script

---
+++ pd/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+_DIR=$(dirname $(readlink -f $0))
+# sleep 10 
+xterm -title "Sensor Processing" -e "pd -nomidi -noaudio $_DIR/Main_sensor-processing.pd" &
+sleep 10
+xterm -title "Audio Processing" -e "pd -nomidi -noadc -rt $_DIR/Main_dsp.pd" &
+# sleep 10
+# xterm -title "Data Plotting" -e "pd -nomidi -noaudio $_DIR/Main_dislpay.pd" &
+
