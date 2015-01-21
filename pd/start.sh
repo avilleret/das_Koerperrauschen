924b0df6c80e6dec964c1854f8119f6f34e0d598
Antoine Villeret <antoine.villeret@gmail.com>
2015-01-21 21:59:05 +0100

    first day

--- pd/start.sh
+++ pd/start.sh
@@ -3,7 +3,7 @@ _DIR=$(dirname $(readlink -f $0))
 # sleep 10 
 xterm -title "Sensor Processing" -e "pd -nomidi -noaudio $_DIR/Main_sensor-processing.pd" &
 sleep 10
-xterm -title "Audio Processing" -e "pd -nomidi -noadc -rt $_DIR/Main_dsp.pd" &
+xterm -title "Audio Processing" -e "pd -r 48000 -blocksize 256 -nomidi -noadc -rt $_DIR/Main_dsp.pd" &
 # sleep 10
 # xterm -title "Data Plotting" -e "pd -nomidi -noaudio $_DIR/Main_dislpay.pd" &
 
